ef37c8e1309796594e6063de4fdc43eab5dc6903
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2022-10-18 23:14:30 -0700

    Initial commit

--- java/sample-apps/aws-sdk/src/main/java/io/opentelemetry/lambda/sampleapps/awssdk/AwsSdkRequestHandler.java
+++ java/sample-apps/aws-sdk/src/main/java/io/opentelemetry/lambda/sampleapps/awssdk/AwsSdkRequestHandler.java
@@ -7,8 +7,12 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.metrics.LongUpDownCounter;
+import io.opentelemetry.api.metrics.DoubleHistogram;
 import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.metrics.ObservableLongCounter;
+import io.opentelemetry.api.metrics.ObservableLongGauge;
+import io.opentelemetry.api.metrics.ObservableLongUpDownCounter;
+import java.util.concurrent.ThreadLocalRandom;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -17,23 +21,53 @@ import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
 public class AwsSdkRequestHandler
     implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+  public static long queueSizeChange;
+  public static long apiBytesSent;
   private static final Logger logger = LogManager.getLogger(AwsSdkRequestHandler.class);
-  private static final Meter sampleMeter =
+  private static final Meter meter =
       GlobalOpenTelemetry.getMeterProvider()
-          .meterBuilder("aws-otel")
+          .meterBuilder("adot")
           .setInstrumentationVersion("1.0")
-          .build();
-  private static final LongUpDownCounter queueSizeCounter =
-      sampleMeter
-          .upDownCounterBuilder("queueSizeChange")
-          .setDescription("Queue Size change")
-          .setUnit("one")
           .build();
 
   private static final AttributeKey<String> API_NAME = AttributeKey.stringKey("apiName");
   private static final AttributeKey<String> STATUS_CODE = AttributeKey.stringKey("statuscode");
   private static final Attributes METRIC_ATTRIBUTES =
       Attributes.builder().put(API_NAME, "apiName").put(STATUS_CODE, "200").build();
+
+  private static final ObservableLongUpDownCounter queueSizeCounter =
+      meter
+          .upDownCounterBuilder("queueSizeChange")
+          .setDescription("Queue Size change")
+          .setUnit("one")
+          .buildWithCallback(measurement -> measurement.record(queueSizeChange, METRIC_ATTRIBUTES));
+
+  private static final ObservableLongGauge totalApiBytesSentMetric =
+      meter
+          .gaugeBuilder("totalApiBytesSentMetricName")
+          .setDescription("Testing Guage")
+          .setUnit("ms")
+          .ofLongs()
+          .buildWithCallback(
+              measurement -> {
+                measurement.record(queueSizeChange, METRIC_ATTRIBUTES);
+              });
+
+  // building synchronous request-based counter metric
+  private static final ObservableLongCounter testCounter =
+      meter
+          .counterBuilder("apiBytesSentMetricName")
+          .setDescription("API request load sent in bytes")
+          .setUnit("1")
+          .buildWithCallback(measurement -> measurement.record(apiBytesSent, METRIC_ATTRIBUTES));
+
+  // building histogram request-based metric
+  private static final DoubleHistogram latencyMetric =
+      meter
+          .histogramBuilder("latencyMetricName")
+          .setDescription("API latency time")
+          .setUnit("ms")
+          .build();
 
   @Override
   public APIGatewayProxyResponseEvent handleRequest(
@@ -47,9 +81,10 @@ public class AwsSdkRequestHandler
           "Hello lambda - found " + listBucketsResponse.buckets().size() + " buckets.");
     }
 
-    // Generate a sample counter metric using the OpenTelemetry Java Metrics API
-    queueSizeCounter.add(2, METRIC_ATTRIBUTES);
-
+    // Generate sample metrics using OTel-Java
+    apiBytesSent = input.toString().length() + ThreadLocalRandom.current().nextLong(100);
+    queueSizeChange = ThreadLocalRandom.current().nextLong(100);
+    latencyMetric.record(System.currentTimeMillis(), METRIC_ATTRIBUTES);
     return response;
   }
 }
